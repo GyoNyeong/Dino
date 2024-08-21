570027db2828289f783358fae8e499dac7356cbd
GyoNyeong <rysud123@naver.com>
2024-08-21 15:29:09 +0900

    first

---
+++ Source/Dino/Public/Obstacle.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Obstacle.generated.h"
+
+
+
+UCLASS()
+class DINO_API AObstacle : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AObstacle();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* StaticMeshComponent;
+
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* BoxComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* DefaultScenComponent;
+
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor);
+
+
+};
